c33a337221b04c451625796b72ff92fcd2f22551
huayu <huayu@DESKTOP-N5K2GI1>
2020-12-05 12:38:01 +0800

    aaaa

--- src/main/java/com/example/demo/TTT.java
+++ src/main/java/com/example/demo/TTT.java
@@ -1,12 +1,26 @@
 package com.example.demo;
 
+import java.io.UnsupportedEncodingException;
+
 public class TTT {
 
 	public static void main(String[] args) {
-		String x = "445444j";
-		x = x.substring(2, x.length());
-		System.out.println(x);
+		// String x = "445444j";
+		// x = x.substring(2, x.length());
+		// System.out.println(x);
 
+		byte[] srcBytes = new byte[] { 2, 4, 8, 1, 5, 6, 7, 10, 15, 50 };
+		byte[] destBytes = new byte[10];
+		System.out.println(destBytes);
+		System.arraycopy(srcBytes, 5, destBytes, 0, 3);
+		String x = null;
+		try {
+			x = new String(destBytes, "utf-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(x);
 	}
 
 }
